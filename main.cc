8ce16fa86cae0d0111d81e6cef83bc267cdcdb3c
Collin Blumenauer <92476364+CollinInMySternum@users.noreply.github.com>
2026-03-19 12:21:46 -0400

    Updates

--- main.cc
+++ main.cc
@@ -1,12 +1,63 @@
-#include "category.h"
+#include "task.h"
 
 #include <cstdlib>
 #include <vector>
+#include <map>
 
-std::vector<Category>;
+Category TopCategory;
+
+using namespace std;
 
 int main() {
     cout << "Hello, welcome to the app!";
 
+    cout << "Open or Create a Category.";
+    cout << "Open Categories: ";
 
+    Category* CurrentCategory = TopCategory;
+
+    while(true) {
+        
+        for(int i = 0; i < Categories.size(); i++) {
+            cout << i << ": " << Category.key << "\n";
+        }
+    
+        string Command;
+        cin >> command;
+        
+        if(command == "open") {
+            cout << "Open Category by Name: ";
+            string CategoryName;
+
+            cin >> CategoryName;
+
+            if(CurrentCategory.Categories.Contains(CategoryName)) {
+                CurrentCategory = CurrentCategory.Categories[CategoryName];
+            }
+
+            else {
+                cout << "Category not found.";
+            }
+        }
+    
+        if(command == ("close")) {
+            CurrentCategory = CurrentCategory.OwningCategory;
+
+            cout << "Category Closed.";
+        }
+
+        if(command == ("create")) {
+            cout << "Name this Category: ";
+            
+            string CategoryName;
+            cin >> CategoryName;
+            
+            cout << "New Category Created: " << CategoryName;
+            
+            Category* NewCategory = new Category;
+            NewCategory->OwningCategory = CurrentCategory;
+
+            CurrentCategory.Categories.insert({CategoryName, NewCategory});
+        }
+    }
 }
